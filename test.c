db77c5414bc91120ead941a624b6e34ce46818b4
jkasze <jkasze@126.com>
2021-11-14 23:30:04 +0800

    add string test cases

--- test.c
+++ test.c
@@ -19,35 +19,47 @@ static int test_pass = 0;
     } while(0)
 
 #define EXPECT_EQ_INT(expect, actual) EXPECT_EQ_BASE((expect) == (actual), expect, actual, "%d")
-#define EXPECT_EQ_DOUBLE(expect,actual) EXPECT_EQ_BASE((expect)== (actual), expect, actual, "%.17g")
+#define EXPECT_EQ_DOUBLE(expect, actual) EXPECT_EQ_BASE((expect) == (actual), expect, actual, "%.17g")
+#define EXPECT_EQ_STRING(expect, actual, alength) \
+    EXPECT_EQ_BASE(sizeof(expect) - 1 == alength && memcmp(expect, actual, alength) == 0, expect, actual, "%s")
+#define EXPECT_TRUE(actual) EXPECT_EQ_BASE((actual) != 0, "true", "false", "%s")
+#define EXPECT_FALSE(actual) EXPECT_EQ_BASE((actual) == 0, "false", "true", "%s")
 
 static void test_parse_null() {
     chita_value v;
-    v.type = CHITA_FALSE;
+    chita_init(&v);
+    chita_set_boolean(&v,0);
     EXPECT_EQ_INT(CHITA_PARSE_OK, chita_parse(&v, "null"));
     EXPECT_EQ_INT(CHITA_NULL, chita_get_type(&v));
+    chita_free(&v);
 }
 
 static void test_parse_true(){
     chita_value v;
-    v.type = CHITA_FALSE;
+    chita_init(&v);
+    chita_set_boolean(&v,0);
     EXPECT_EQ_INT(CHITA_PARSE_OK,chita_parse(&v,"true"));
     EXPECT_EQ_INT(CHITA_TRUE,chita_get_type(&v));
+    chita_free(&v);
 }
 
 static void test_parse_false(){
     chita_value v;
-    v.type = CHITA_TRUE;
+    chita_init(&v);
+    chita_set_boolean(&v,1);
     EXPECT_EQ_INT(CHITA_PARSE_OK,chita_parse(&v,"false"));
     EXPECT_EQ_INT(CHITA_FALSE,chita_get_type(&v));
+    chita_free(&v);
 }
 
-#define TEST_NUMBER(expect,json)\
-    do{\
+#define TEST_NUMBER(expect, json)\
+    do {\
         chita_value v;\
-        EXPECT_EQ_INT(CHITA_PARSE_OK,chita_parse(&v,json));\
-        EXPECT_EQ_INT(CHITA_NUMBER,chita_get_type(&v));\
-        EXPECT_EQ_DOUBLE(expect,chita_get_number(&v));\
+        chita_init(&v);\
+        EXPECT_EQ_INT(CHITA_PARSE_OK, chita_parse(&v, json));\
+        EXPECT_EQ_INT(CHITA_NUMBER, chita_get_type(&v));\
+        EXPECT_EQ_DOUBLE(expect, chita_get_number(&v));\
+        chita_free(&v);\
     } while(0)
 
 static void test_parse_number() {
@@ -82,12 +94,24 @@ static void test_parse_number() {
     TEST_NUMBER(-1.7976931348623157e+308, "-1.7976931348623157e+308");
 }
 
+#define TEST_STRING(expect, json)\
+    do{\
+    } while(0)
+
+static void test_parse_string(){
+    TEST_STRING("", "\"\"");
+    TEST_STRING("Hello", "\"Hello\"");
+    TEST_STRING("Hello\nWorld", "\"Hello\\nWorld\"");
+    TEST_STRING("\" \\ / \b \f \n \r \t", "\"\\\" \\\\ \\/ \\b \\f \\n \\r \\t\"");
+}
 #define TEST_ERROR(error,json)\
     do{\
         chita_value v;\
+        chita_init(&v);\
         v.type = CHITA_FALSE;\
         EXPECT_EQ_INT(error,chita_parse(&v,json));\
         EXPECT_EQ_INT(CHITA_NULL,chita_get_type(&v));\
+        chita_free(&v);\
     } while(0)
 
 static void test_parse_expect_value() {
@@ -124,15 +148,81 @@ static void test_parse_number_too_big(){
     TEST_ERROR(CHITA_PARSE_NUMBER_TOO_BIG,"-1e309");
 }
 
+static void test_parse_missing_quotation_mark() {
+    TEST_ERROR(CHITA_PARSE_MISS_QUOTATION_MARK, "\"");
+    TEST_ERROR(CHITA_PARSE_MISS_QUOTATION_MARK, "\"abc");
+}
+
+static void test_parse_invalid_string_escape() {
+    TEST_ERROR(CHITA_PARSE_INVALID_STRING_ESCAPE, "\"\\v\"");
+    TEST_ERROR(CHITA_PARSE_INVALID_STRING_ESCAPE, "\"\\'\"");
+    TEST_ERROR(CHITA_PARSE_INVALID_STRING_ESCAPE, "\"\\0\"");
+    TEST_ERROR(CHITA_PARSE_INVALID_STRING_ESCAPE, "\"\\x12\"");
+}
+
+static void test_parse_invalid_string_char() {
+    TEST_ERROR(CHITA_PARSE_INVALID_STRING_CHAR, "\"\x01\"");
+    TEST_ERROR(CHITA_PARSE_INVALID_STRING_CHAR, "\"\x1F\"");
+}
+
+static void test_access_null() {
+    chita_value v;
+    chita_init(&v);
+    chita_set_string(&v, "a", 1);
+    chita_set_null(&v);
+    EXPECT_EQ_INT(CHITA_NULL, chita_get_type(&v));
+    chita_free(&v);
+}
+
+static void test_access_boolean(){
+    chita_value v;
+    chita_init(&v);
+    chita_set_string(&v,"a",1);
+    chita_set_boolean(&v,1);
+    EXPECT_TRUE(chita_get_boolean(&v));
+    chita_set_boolean(&v,0);
+    EXPECT_FALSE(chita_get_boolean(&v));
+    chita_free(&v);
+}
+
+static void test_access_number(){
+    chita_value v;
+    chita_init(&v);
+    chita_set_string(&v,"a",2);
+    chita_set_number(&v,1234.5);
+    EXPECT_EQ_DOUBLE(1234.5,chita_get_number(&v));
+    chita_free(&v);
+}
+
+static void test_access_string(){
+    chita_value v;
+    chita_init(&v);
+    chita_set_string(&v,"",0);
+    EXPECT_EQ_STRING("",chita_get_string(&v),chita_get_string_length(&v));
+    chita_set_string(&v,"Hello",5);
+    EXPECT_EQ_STRING("Hello",chita_get_string(&v),
+                        chita_get_string_length(&v));
+    chita_free(&v);
+}
+
 static void test_parse() {
     test_parse_null();
-    test_parse_true();    
-    test_parse_false(); 
-    test_parse_number();  
+    test_parse_true();
+    test_parse_false();
+    test_parse_number();
+    test_parse_string();
     test_parse_expect_value();
     test_parse_invalid_value();
     test_parse_root_not_singular();
     test_parse_number_too_big();
+    test_parse_missing_quotation_mark();
+    test_parse_invalid_string_escape();
+    test_parse_invalid_string_char();
+
+    test_access_null();
+    test_access_boolean();
+    test_access_number();
+    test_access_string();
 }
 
 int main() {
